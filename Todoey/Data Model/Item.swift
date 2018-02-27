41f0507a6a21f3322822a140df6da6a28f80f86a
aholly38 <alainholly@gmail.com>
2018-02-26 20:19:50 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alain on 2/25/18.
+//  Copyright © 2018 Alain Holly. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+  
+}
